fa6ffba68b48ed52f27bc7c092b02b7979389932
Sasheem <sts12@me.com>
2019-09-18 15:35:46 -0400

    organized file architecture

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Directory from '../../components/directory/directory.component.jsx';
+
 import './homepage.styles.scss';
 
 // making this a functional component 
@@ -14,32 +16,24 @@ const HomePage = () => (
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>JACKETS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>SNEAKERS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>WOMEN</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>MEN</h1>
