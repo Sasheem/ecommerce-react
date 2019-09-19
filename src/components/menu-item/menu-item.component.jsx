5763573e503b5cbbf53405fdb47db48a8ea3d9aa
Sasheem <sts12@me.com>
2019-09-19 10:35:27 -0400

    updated app to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+// higher order component
+// function that takes a component as an arg
+// then returns a modified component
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
 // functional component, don't need state right now
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => {
+        /**
+         * * remember match has the base url and your appending the
+         * * menu item name to route to appropriate page
+         */
+        history.push(`${match.url}${linkUrl}`);
+		console.log(history);
+        
+    }}>
         <div 
             className='background-image' 
             style={{
@@ -18,4 +30,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+// withRoute()
+// this will return a super powered MenuItem that has
+// props from Route: history, location and  match
+export default withRouter(MenuItem);
