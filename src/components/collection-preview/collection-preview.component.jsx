384b17a4586dfc6d9971546a300fc1e0489690db
Sasheem <sts12@me.com>
2019-09-19 11:58:20 -0400

    added shop page, collection preview, and collection item components

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component.jsx';
+
+import './collection-preview.styles.scss';
+
+/**
+ * ? keep in mind this can be expensive on performance
+ * ? if the items array is large 
+ * */ 
+
+const CollectionPreview = ({ title, items }) => (
+	<div className='collection-preview'>
+		<h1>{title.toUpperCase()}</h1>
+		<div className='preview'>
+			{items
+				.filter((item, idx) => idx < 4)
+				.map(({ id, ...otherItemProps }) => (
+					<CollectionItem key={id} {...otherItemProps} />
+				))}
+		</div>
+	</div>
+);
+
+export default CollectionPreview;
