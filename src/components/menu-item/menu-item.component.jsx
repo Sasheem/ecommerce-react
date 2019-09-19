6527adce55c4d37d8e83240d8fe332c2b320f075
Sasheem <sts12@me.com>
2019-09-18 23:00:48 -0400

    fininished styling menu-item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 // functional component, don't need state right now
-const MenuItem = ({ title }) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+        <div 
+            className='background-image' 
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }} 
+        />
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -9,38 +9,7 @@ import './homepage.styles.scss';
 // methods or state right now
 const HomePage = () => (
     <div className='homepage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>HATS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>JACKETS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>SNEAKERS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>WOMEN</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>MEN</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+        <Directory />
     </div>
 );
 
