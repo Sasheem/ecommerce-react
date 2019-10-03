4bc8a06789adb776ead6fdfb394874ff9c54e1e3
Sasheem <sts12@me.com>
2019-10-03 11:32:44 -0400

    added styled-components, converted some components from scss to styled-components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component.jsx';
 
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
 // making this a functional component 
 // because I don't need any lifecycle 
 // methods or state right now
 const HomePage = () => (
-    <div className='homepage'>
-        <Directory />
-    </div>
+	<HomePageContainer>
+		<Directory />
+	</HomePageContainer>
 );
 
 export default HomePage;
