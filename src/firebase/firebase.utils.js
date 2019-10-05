7209a412033eb1373424cabecc03d5a455605fb2
Sasheem <sts12@me.com>
2019-10-05 13:01:22 -0400

    moved shop data to firebase, converted redux flow to grab data from firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,8 @@ const config = {
 	appId: '1:633632461527:web:db4ba516afc46284039692'
 };
 
+firebase.initializeApp(config);
+
 // async action because we are making an API call
 /**
  * * check for any data
@@ -42,7 +44,62 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 	return userRef;
 };
 
-firebase.initializeApp(config);
+/**
+ *
+ * @param {id for key} collectionKey
+ * @param {could be an array of objects or object of objects?} objectsToAdd
+ */
+export const addCollectionAndDocuments = async (
+	collectionKey,
+	objectsToAdd
+) => {
+	// create the collection in firestore using our collection key
+	const collectionRef = firestore.collection(collectionKey);
+
+	// group our calls into one big request
+	// in case something happens to interrupt the internet connection or anything
+	const batch = firestore.batch();
+	objectsToAdd.forEach(obj => {
+		// tells firebase, give us a new doc ref in this collection and
+		// randomly generate an ID for me
+		const newDocRef = collectionRef.doc();
+
+		// set it on batch so we know it will be set or not set
+		// we know it won't be partially set
+		batch.set(newDocRef, obj);
+	});
+
+	// now send batch request
+	// returns a promise, on success it resolves a void value
+	return await batch.commit();
+};
+
+// convert collections array to an object of objects
+// where the title for each collection object is the key
+// and the data is the respective collection object
+export const convertCollectionsSnapshotToMap = collections => {
+	const transformedCollections = collections.docs.map(doc => {
+		const { title, items } = doc.data();
+
+		// encodeURI takes a string and converts it to a string to be used for URLs
+		return {
+			routeName: encodeURI(title.toLowerCase()),
+			id: doc.id,
+			title,
+			items
+		};
+	});
+
+	return transformedCollections.reduce((accumulator, collection) => {
+		// creates an empty object with the name of the current iteration we are on
+		// and then sets that equal to the collection for the iteration we are on
+		/**
+		 * * confused me at first because this looks like an array index terminology
+		 */
+		accumulator[collection.title.toLowerCase()] = collection;
+		return accumulator;
+	}, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
